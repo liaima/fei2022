899f4435b3153e453176373980c632f8d71f6e65
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-04-10 18:39:26 -0300

    Crear archivo docker-compose.dist.yml

--- bin/first_start.sh
+++ bin/first_start.sh
@@ -2,6 +2,10 @@
 
 docker-compose exec app composer install
 
+echo "CAMBIANDO PERMISOS: ...."
+
 docker-compose exec app chmod 777 ./web/assets -R
 
 docker-compose exec app chmod 777 ./runtime -R
+
+echo "PERMISOS MODIFICADOS!"
