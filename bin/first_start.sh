c7d547244abdb85fec30adac821485c7cc8001dd
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-04-19 17:51:39 -0300

    modificación de propietario carpeta migrations

--- bin/first_start.sh
+++ bin/first_start.sh
@@ -4,8 +4,8 @@ docker-compose exec app composer install
 
 echo "CAMBIANDO PERMISOS: ...."
 
-docker-compose exec app chmod 777 ./web/assets -R
-
-docker-compose exec app chmod 777 ./runtime -R
-
-echo "PERMISOS MODIFICADOS!"
+docker-compose exec app chmod 777 ./web/assets -R &&\
+  echo "+Permiso modificado a ./web/assets" &&\
+  docker-compose exec app chmod 777 ./runtime -R && \
+  echo "+Permoso modificado a ./runtime" &&\
+  echo "PERMISOS MODIFICADOS CORRECTAMENTE!"
